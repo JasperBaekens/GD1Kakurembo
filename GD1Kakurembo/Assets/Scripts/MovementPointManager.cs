472d3705b61ba30d9ab91c305746cfc4f0915fd5
JasperBaekens <jasper.baekens@hotmail.com>
2024-05-12 23:39:59 +0200

    Demo upload

--- GD1Kakurembo/Assets/Scripts/MovementPointManager.cs
+++ GD1Kakurembo/Assets/Scripts/MovementPointManager.cs
@@ -2,6 +2,8 @@ using System;
 using System.CodeDom.Compiler;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
 using UnityEngine;
 
 public class MovementPointManager : MonoBehaviour
@@ -31,6 +33,11 @@ public class MovementPointManager : MonoBehaviour
     public float motivationMaxPlayer1 = 100 ;
     public float motivationCurrentPlayer1 = 100;
 
+    public List<GameObject> tilesToPingPlayer1;
+    public List<GameObject> PingsPlayer1;
+    public List<GameObject> tilesToPingPlayer1LastFrame;
+
+
     //player2
     public bool myTurn2;
     public bool previousFrameMyTurn2 = false;
@@ -56,14 +63,22 @@ public class MovementPointManager : MonoBehaviour
     public float motivationMaxPlayer2 = 100;
     public float motivationCurrentPlayer2 = 100;
 
+    public List<GameObject> tilesToPingPlayer2;
+    public List<GameObject> PingsPlayer2;
+    public List<GameObject> tilesToPingPlayer2LastFrame;
+
+
 
     //overal
+    public GameObject PrefabPing;
     public int TurnCounter;
 
     public bool Player1Won = false;
     public bool Player2Won = false;
 
     public float MotivationDecreasePerTurn = 5;
+    public bool Commander1Found = false;
+    public bool Commander2Found = false;
 
     private void Update()
     {
@@ -80,7 +95,112 @@ public class MovementPointManager : MonoBehaviour
         CheckWinPlayer1();
         CheckWinPlayer2();
 
+        ClearPingListPlayer1();
+        ClearPingListPlayer2();
 
+        CheckForScoutCommanderInteraction();
+
+        DrawPingListPlayer1();
+        DrawPingListPlayer2();
+
+
+    }
+
+    private void CheckForScoutCommanderInteraction()
+    {
+        if (currentFrameFirstInTurn1 || currentFrameFirstInTurn2)
+        {
+            Commander1Found = false;
+            Commander2Found = false;
+
+        }
+
+
+
+        if (!Commander1Found & (
+            CommanderPlayer1.GetComponent<CharacterProperties>().currentTile == Spy1Player2.GetComponent<CharacterProperties>().currentTile ||
+            CommanderPlayer1.GetComponent<CharacterProperties>().currentTile == Spy2Player2.GetComponent<CharacterProperties>().currentTile ||
+            CommanderPlayer1.GetComponent<CharacterProperties>().currentTile == Spy3Player2.GetComponent<CharacterProperties>().currentTile
+            ))
+        {
+            Commander1Found = true;
+            tilesToPingPlayer2.Add(CommanderPlayer1.GetComponent<CharacterProperties>().currentTile);
+            tilesToPingPlayer1.Add(CommanderPlayer1.GetComponent<CharacterProperties>().currentTile);
+
+        }
+        if (!Commander2Found &(
+            CommanderPlayer2.GetComponent<CharacterProperties>().currentTile == Spy1Player1.GetComponent<CharacterProperties>().currentTile ||
+            CommanderPlayer2.GetComponent<CharacterProperties>().currentTile == Spy2Player1.GetComponent<CharacterProperties>().currentTile ||
+            CommanderPlayer2.GetComponent<CharacterProperties>().currentTile == Spy3Player1.GetComponent<CharacterProperties>().currentTile
+            ))
+        {
+            Commander2Found = true;
+            tilesToPingPlayer2.Add(CommanderPlayer2.GetComponent<CharacterProperties>().currentTile);
+            tilesToPingPlayer1.Add(CommanderPlayer2.GetComponent<CharacterProperties>().currentTile);
+        }
+
+
+
+    }
+
+    private void DrawPingListPlayer1()
+    {
+        if (currentFrameFirstInTurn1 ||(myTurn1 && tilesToPingPlayer1.Count > tilesToPingPlayer1LastFrame.Count))
+        {
+            DeletePings(PingsPlayer1);
+            foreach (var tile in tilesToPingPlayer1) 
+            { 
+                GameObject Ping = Instantiate(PrefabPing);
+                Ping.transform.position = tile.transform.position;
+                PingsPlayer1.Add(Ping);
+            }
+        }
+        tilesToPingPlayer1LastFrame = tilesToPingPlayer1;
+    }
+
+    private void DrawPingListPlayer2()
+    {
+        if (currentFrameFirstInTurn2 || (myTurn2 && tilesToPingPlayer2.Count > tilesToPingPlayer2LastFrame.Count))
+        {
+            DeletePings(PingsPlayer2);
+            foreach (var tile in tilesToPingPlayer2)
+            {
+                GameObject Ping = Instantiate(PrefabPing);
+                Ping.transform.position = tile.transform.position;
+                PingsPlayer2.Add(Ping);
+            }
+        }
+        tilesToPingPlayer2LastFrame = tilesToPingPlayer2;
+    }
+
+
+    private void ClearPingListPlayer1()
+    {
+        if (currentFrameFirstInTurn2)
+        {
+            DeletePings(PingsPlayer1);
+            PingsPlayer1.Clear();
+            tilesToPingPlayer1.Clear();
+        }
+
+    }
+
+
+    private void ClearPingListPlayer2()
+    {
+        if (currentFrameFirstInTurn1)
+        {
+            DeletePings(PingsPlayer2);
+            tilesToPingPlayer2.Clear();
+            PingsPlayer2.Clear();
+        }
+    }
+    private void DeletePings(List<GameObject> pingsPlayer)
+    {
+        for (int i = pingsPlayer.Count-1; i >= 0; i--)
+        {
+            GameObject.Destroy(pingsPlayer[i]);
+        }
     }
 
     private void UpdateMotivation()
