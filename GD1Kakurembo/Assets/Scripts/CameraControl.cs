e848b396c95d4e6f03e85d500936b3649cf24cfb
JasperBaekens <jasper.baekens@hotmail.com>
2024-05-15 16:22:24 +0200

    CameraFixLimits

--- GD1Kakurembo/Assets/Scripts/CameraControl.cs
+++ GD1Kakurembo/Assets/Scripts/CameraControl.cs
@@ -12,10 +12,13 @@ public class BoundaryControl : MonoBehaviour
     public float TranslateX;
     public float TranslateZ;
     public Vector3 TranslateXZ;
+    public Vector3 NewPositionLimitTest;
+    public Vector3 NewPositionMax;
+    public Vector3 NewPositionMaxMin;
     public Vector3 NewPosition;
     public Vector3 MousePosition;
     public float MouseMargin = 0.1f;
-
+    public float cameraHeight = 3f;
 
     // Update is called once per frame
     void Update()
@@ -24,10 +27,7 @@ public class BoundaryControl : MonoBehaviour
 
         TranslateZ = Input.GetAxis("Vertical");
 
-        TranslateXZ = new Vector3(TranslateX, 0, TranslateZ).normalized * Speed * Time.deltaTime;
-
-        NewPosition = transform.position + TranslateXZ; //new Vector3(TranslateX, 0, TranslateZ);
-
+        TranslateXZ = new Vector3(TranslateZ, 0, -TranslateX).normalized * Speed * Time.deltaTime;
 
 
         if (TranslateXZ == Vector3.zero)
@@ -35,39 +35,40 @@ public class BoundaryControl : MonoBehaviour
             MoveWithScreen();
         }
 
+        NewPositionLimitTest = transform.position + TranslateXZ; //new Vector3(TranslateX, 0, TranslateZ);
+        NewPositionMax = new Vector3(Math.Min(NewPositionLimitTest.x, LimitX.x), cameraHeight, Math.Min(NewPositionLimitTest.z, LimitZ.z));
+        NewPositionMaxMin = new Vector3(Math.Max(NewPositionMax.x, -LimitX.x), cameraHeight, Math.Max(NewPositionMax.z, -LimitZ.z));
 
+        transform.position = NewPositionMaxMin;
+        //if ((NewPosition.x >= LimitX.x))
+        //{
+        //    //Debug.Log("out of X+");
+        //    TranslateXZ = new Vector3(TranslateX, 0, Math.Min(0, TranslateZ)).normalized * Speed * Time.deltaTime;
+        //    NewPosition = transform.position + TranslateXZ; //new Vector3(TranslateX, 0, TranslateZ);
 
-        if ((NewPosition.x >= LimitX.x))
-        {
-            //Debug.Log("out of X+");
-            TranslateXZ = new Vector3(TranslateX, 0, Math.Min(0, TranslateZ)).normalized * Speed * Time.deltaTime;
-            NewPosition = transform.position + TranslateXZ; //new Vector3(TranslateX, 0, TranslateZ);
+        //}
+        //if (NewPosition.x <= (-LimitX.x))
+        //{
+        //    //Debug.Log("out of X-");
+        //    TranslateXZ = new Vector3(TranslateX, 0, Math.Max(0, TranslateZ)).normalized * Speed * Time.deltaTime;
+        //    NewPosition = transform.position + TranslateXZ; //new Vector3(TranslateX, 0, TranslateZ);
 
+        //}
+        //if ((NewPosition.z >= LimitZ.z))
+        //{
+        //    //Debug.Log("out of Z+");
+        //    TranslateXZ = new Vector3(Math.Max(0, TranslateX), 0, TranslateZ).normalized * Speed * Time.deltaTime;
+        //    NewPosition = transform.position + TranslateXZ; //new Vector3(TranslateX, 0, TranslateZ);
 
-        }
-        else if (NewPosition.x <= (-LimitX.x))
-        {
-            //Debug.Log("out of X-");
-            TranslateXZ = new Vector3(TranslateX, 0, Math.Max(0, TranslateZ)).normalized * Speed * Time.deltaTime;
-            NewPosition = transform.position + TranslateXZ; //new Vector3(TranslateX, 0, TranslateZ);
+        //}
+        //if (NewPosition.z <= (-LimitZ.z))
+        //{
+        //    //Debug.Log("out of Z-");
+        //    TranslateXZ = new Vector3(Math.Min(0, TranslateX), 0, TranslateZ).normalized * Speed * Time.deltaTime;
+        //    NewPosition = transform.position + TranslateXZ; //new Vector3(TranslateX, 0, TranslateZ);
 
-        }
-        else if ((NewPosition.z >= LimitZ.z))
-        {
-            //Debug.Log("out of Z+");
-            TranslateXZ = new Vector3(Math.Max(0, TranslateX), 0, TranslateZ).normalized * Speed * Time.deltaTime;
-            NewPosition = transform.position + TranslateXZ; //new Vector3(TranslateX, 0, TranslateZ);
+        //}
 
-        }
-        else if (NewPosition.z <= (-LimitZ.z))
-        {
-            //Debug.Log("out of Z-");
-            TranslateXZ = new Vector3(Math.Min(0, TranslateX), 0, TranslateZ).normalized * Speed * Time.deltaTime;
-            NewPosition = transform.position + TranslateXZ; //new Vector3(TranslateX, 0, TranslateZ);
-
-        }
-
-        transform.Translate(TranslateXZ);
         //Debug.Log("We Good");
 
 
@@ -99,7 +100,6 @@ public class BoundaryControl : MonoBehaviour
         if (MousePosition.x < MouseMargin)
         {
             TranslateX = -1;
-
         }
 
         if (MousePosition.y < MouseMargin)
@@ -107,10 +107,7 @@ public class BoundaryControl : MonoBehaviour
             TranslateZ = -1;
         }
 
-        TranslateXZ = new Vector3(TranslateX, 0, TranslateZ).normalized * Speed * Time.deltaTime;
-
-        NewPosition = transform.position + TranslateXZ; //new Vector3(TranslateX, 0, TranslateZ);
-
+        TranslateXZ = new Vector3(TranslateZ, 0, - TranslateX).normalized * Speed * Time.deltaTime;
     }
 
 
